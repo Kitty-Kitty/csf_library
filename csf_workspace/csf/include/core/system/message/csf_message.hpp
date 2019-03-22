840a667252142f1a093ce38a7a1e579886d0444a
Kitty-Kitty <fangzhenmu@hikvision.com>
2019-03-22 17:26:23 +0800

    update message

--- csf_workspace/csf/include/core/system/message/csf_message.hpp
+++ csf_workspace/csf/include/core/system/message/csf_message.hpp
@@ -41,6 +41,57 @@ namespace csf
 				{
 
 				public:
+					/**
+					* 表示消息的类型
+					* @author f
+					* @version 1.0
+					* @updated 22-3月-2019 15:22:05
+					*/
+					typedef enum csf_message_type_enum
+					{
+						/**
+						* 表示未知的消息类型
+						*/
+						csf_message_type_none = 0x00000000,
+						/**
+						* 表示添加消息类型
+						*/
+						csf_message_type_add = 0x00010000,
+						/**
+						* 表示删除消息类型
+						*/
+						csf_message_type_delete = 0x00020000,
+						/**
+						* 表示更新消息类型
+						*/
+						csf_message_type_update = 0x00040000,
+						/**
+						* 表示控制消息类型
+						*/
+						csf_message_type_ctrl = 0x00080000,
+						/**
+						* 表示初始化消息类型
+						*/
+						csf_message_type_init = 0x00100000,
+						/**
+						* 表示启动消息类型
+						*/
+						csf_message_type_start = 0x00200000,
+						/**
+						* 表示停止消息类型
+						*/
+						csf_message_type_stop = 0x00400000,
+						/**
+						* 表示创建消息类型
+						*/
+						csf_message_type_create = 0x00800000,
+						/**
+						* 表示销毁消息类型
+						*/
+						csf_message_type_destroy = 0x01000000
+					}csf_message_type;
+
+				public:
 					csf_message();
 					virtual ~csf_message();
 
@@ -78,7 +129,7 @@ namespace csf
 					/**
 					 * 表示消息的类型
 					 */
-					inline csf::core::system::message::csf_message_type get_type() {
+					inline csf_message_type get_type() {
 
 						return m_type;
 					}
@@ -87,7 +138,7 @@ namespace csf
 					 * 
 					 * @param newVal
 					 */
-					inline void set_type(const csf::core::system::message::csf_message_type newVal) {
+					inline void set_type(const csf_message_type newVal) {
 
 						m_type = newVal;
 					}
@@ -116,7 +167,7 @@ namespace csf
 					/**
 					 * 表示消息的类型
 					 */
-					csf::core::system::message::csf_message_type m_type = csf_message_type_none;
+					csf_message_type m_type = csf_message_type_none;
 					/**
 					 * 表示message自带的信息内容
 					 */
--- csf_workspace/csf/include/core/system/message/csf_message_callback.hpp
+++ csf_workspace/csf/include/core/system/message/csf_message_callback.hpp
@@ -20,6 +20,10 @@
 #if !defined(CSF_MESSAGE_CALLBACK_INCLUDED_)
 #define CSF_MESSAGE_CALLBACK_INCLUDED_
 
+#include "csf_base_include.h"
+#include "csf_stdcplusplus.hpp"
+#include "csf_stdbind.hpp"
+
 namespace csf
 {
 	namespace core
@@ -29,19 +33,27 @@ namespace csf
 			namespace message
 			{
 				/**
-				 * 表示消息的回调函数
-				 * @author f
-				 * @version 1.0
-				 * @created 27-6月-2018 17:28:44
-				 */
-				class csf_message_callback
-				{
-
-				public:
-					csf_message_callback();
-					virtual ~csf_message_callback();
-
-				};
+				* 表示定义一个网络连接类
+				* @author fangzhenmu@aliyun.com
+				* @version 1.0
+				* @created 01-10月-2018 12:54:36
+				*/
+				class csf_message;
+				/**
+				* 表示定义一个网络连接智能指针
+				* @author fangzhenmu@aliyun.com
+				* @version 1.0
+				* @created 01-10月-2018 12:54:36
+				*/
+				typedef	csf_shared_ptr<csf_message>				csf_message_ptr;
+				/**
+				* 表示连接的返回函数
+				* @author f
+				* @version 1.0
+				* @created 01-10月-2018 12:54:37
+				*/
+				typedef csf_function<csf_int32(csf_message_ptr
+					, csf_connect_error&)> 	csf_connect_callback;
 
 			}
 
